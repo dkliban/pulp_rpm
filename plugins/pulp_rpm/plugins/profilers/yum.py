72655c2e67ee9e2226b2563f9968e5c820b70101
Dennis Kliban <dkliban@redhat.com>
2016-05-05 15:06:48 -0400

    Improves performance of errata installation.
    Pulp API no longer translates errata into a list of packages applicable to a consumer. Errata
    ids are passed to the agent on the consumer where yum and security plugin take care of the
    translation. The new functionality is equivalent to running
    
        yum update-minimal --advisories=<errata_id>,<another_id>
    
    on the consumer.
    
    closes #1462
    https://pulp.plan.io/issues/1462

--- plugins/pulp_rpm/plugins/profilers/yum.py
+++ plugins/pulp_rpm/plugins/profilers/yum.py
@@ -96,10 +96,7 @@ class YumProfiler(Profiler):
                 if TYPE_ID_RPM not in consumer.profiles:
                     reason = _('Consumer has no RPM unit profile')
                     raise InvalidUnitsRequested(units, reason)
-                values, upgrade_details = YumProfiler._translate_erratum(
-                    unit, conduit.get_bindings(consumer.id), consumer, conduit)
-                if values:
-                    translated_units.extend(values)
+                translated_units.append(unit)
         return translated_units
 
     @staticmethod
